c64c4aec46826f0247f926dca67becdda28333b0
BobrCRV <152267249+BobrCRV@users.noreply.github.com>
2024-10-01 19:42:03 +0200

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,17 +1,12 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 exports.handler = async (event) => {
     try {
         const response = await axios.get('https://resources.curve.fi/crvusd/liquidations/#hard-liquidation-example');
-        const $ = cheerio.load(response.data);
-        
-        // Извлекаем родительский div
-        const divElement = $('div#centered2').parent().html(); // Извлекаем содержимое родителя
 
         return {
             statusCode: 200,
-            body: divElement,
+            body: response.data, // Возвращаем весь HTML-код страницы
         };
     } catch (error) {
         return {

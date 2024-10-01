5a9b1bf29cd28239b5bbb84efe81dee4f7623768
BobrCRV <152267249+BobrCRV@users.noreply.github.com>
2024-10-01 19:09:31 +0200

    Create proxy.js

---
+++ functions/proxy.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+exports.handler = async (event) => {
+    try {
+        const response = await axios.get('https://resources.curve.fi/crvusd/liquidations/#hard-liquidation-example');
+        const $ = cheerio.load(response.data);
+        const canvasElement = $('#crvHardLiq').toString();
+
+        return {
+            statusCode: 200,
+            body: canvasElement,
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Ошибка при загрузке страницы.',
+        };
+    }
+};

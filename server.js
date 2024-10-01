1b5870bab39ddd742f5a5db4518aaa486b09b38f
BobrCRV <152267249+BobrCRV@users.noreply.github.com>
2024-10-01 19:01:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const app = express();
-const port = 3000;
-
-// Прокси для получения содержимого с другого сайта
-app.get('/proxy', async (req, res) => {
+exports.handler = async (event) => {
     try {
-        // Получаем содержимое целевой страницы
-        const { data } = await axios.get('https://resources.curve.fi/crvusd/liquidations/#hard-liquidation-example');
-        
-        // Парсим HTML с помощью Cheerio
-        const $ = cheerio.load(data);
-        
-        // Извлекаем нужный элемент <canvas>
+        const response = await axios.get('https://resources.curve.fi/crvusd/liquidations/#hard-liquidation-example');
+        const $ = cheerio.load(response.data);
         const canvasElement = $('#crvHardLiq').toString();
 
-        // Отправляем найденный элемент в ответе
-        res.send(canvasElement);
+        return {
+            statusCode: 200,
+            body: canvasElement,
+        };
     } catch (error) {
-        res.status(500).send('Ошибка при загрузке страницы.');
+        return {
+            statusCode: 500,
+            body: 'Ошибка при загрузке страницы.',
+        };
     }
-});
-
-// Запуск сервера
-app.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}`);
-});
-
+};
